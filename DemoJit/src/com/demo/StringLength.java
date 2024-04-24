b4e3852a1a9eb20624826400abfd614960e6174d
Wisilica <Wisilica@DESKTOP-ACF6RH4>
2024-04-24 12:07:13 +0530

    commit second

---
+++ DemoJit/src/com/demo/StringLength.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String name= "Sarath";
+		int len= name.length();
+		System.out.println(len);
+
+	}
+
+}
