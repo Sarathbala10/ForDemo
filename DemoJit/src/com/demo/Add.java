facb74841fcd132c44dcf90c98fb4935e1bf62fb
Wisilica <Wisilica@DESKTOP-ACF6RH4>
2024-04-24 12:02:03 +0530

    first commit

--- DemoJit/src/com/demo/Add.java
+++ DemoJit/src/com/demo/Add.java
@@ -1,13 +1,14 @@
-package sample;
+package com.demo;
 
 public class Add {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 		int a=10;
 		int b=20;
-		int c=a+b;
+		int c= a+b;
 		System.out.println(c);
-
 	}
 
 }
